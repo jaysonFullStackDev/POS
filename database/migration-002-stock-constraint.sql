2f0f51e497a570df35b1718a31aafe793f7d4b2a
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-04-13 11:21:18 +0800

    chore: remove backend and database - moved to separate repo (BrewPOS-Backend)

--- database/migration-002-stock-constraint.sql
+++
@@ -1,5 +0,0 @@
--- Migration 002: Prevent negative stock (race condition fix)
--- Run: psql -U postgres -d brewpos -f database/migration-002-stock-constraint.sql
-
-ALTER TABLE ingredients
-  ADD CONSTRAINT stock_qty_non_negative CHECK (stock_qty >= 0);
